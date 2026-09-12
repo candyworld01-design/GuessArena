dc04e973bc3ee832068d0fbc7ae905acf9920e09
candyworld01-design <familywale01@gmail.com>
2026-09-12 10:19:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,21 @@
 import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return 'Bot Alive'
+
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+
+
+Thread(target=run).start()
+import os
 import random
 import sqlite3
 import time
